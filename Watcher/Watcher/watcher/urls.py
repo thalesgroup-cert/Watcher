268b9e5cb6a1700d91eb794c1656f4a92db13570
Felix83000 <flix.dax@gmail.com>
2021-12-09 15:09:39 +0100

    Upgrade to 'path' instead of 'url' & downgrade APScheduler due to Warnings
    'url' is removed on Django 4.0

--- Watcher/Watcher/watcher/urls.py
+++ Watcher/Watcher/watcher/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    url('', include('frontend.urls')),
-    url('', include('threats_watcher.urls')),
-    url('', include('data_leak.urls')),
-    url('', include('site_monitoring.urls')),
-    url('', include('dns_finder.urls')),
-    url('', include('accounts.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('', include('frontend.urls')),
+    path('', include('threats_watcher.urls')),
+    path('', include('data_leak.urls')),
+    path('', include('site_monitoring.urls')),
+    path('', include('dns_finder.urls')),
+    path('', include('accounts.urls')),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/', admin.site.urls),
 ]
