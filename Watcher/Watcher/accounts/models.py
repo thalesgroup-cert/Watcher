97f53364c286f9a88cc0ae6147bff01d0f6746e0
NEZRI Ygal <T0298741@gemalto.com>
2024-07-22 23:41:30 +0200

    API Key Creation & Management
    Added functionality for superusers and users to create and manage API keys, with Knox integration for secure key hashing.

--- Watcher/Watcher/accounts/models.py
+++ Watcher/Watcher/accounts/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django_auth_ldap.backend import populate_user
 from django.contrib.auth.models import User
-from knox.models import AuthToken
-
 
 class APIKey(models.Model):
-    """
-    Manages creation, modification, and deletion of user API keys.
-    """
-    auth_token = models.OneToOneField(AuthToken, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    key = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expiration = models.IntegerField(default=30)
+    expiry_at = models.DateTimeField(null=True, blank=True)
+    key_details = models.TextField(null=True, blank=True)  # Ajout de ce champ
 
     def __str__(self):
-        return f"API Key for {self.auth_token.user.username}"
+        return f"API Key for {self.user.username}"
 
     class Meta:
         verbose_name = "API Key"
@@ -22,5 +22,6 @@ class APIKey(models.Model):
 def make_inactive(sender, user, **kwargs):
     if not User.objects.filter(username=user.username):
         user.is_active = False
+
 
 populate_user.connect(make_inactive)
