afb6cc9a6ee3864ba43f2ed115a5eecf6b7a061a
ygalnezri <ygalnezri@icloud.com>
2025-12-08 18:24:03 +0100

    Update tests

--- Watcher/Watcher/threats_watcher/tests.py
+++ Watcher/Watcher/threats_watcher/tests.py
@@ -164,8 +164,9 @@ class APITest(APITestCase):
         response = self.client.get(url)
         
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['name'], f"phishing-{self.unique_id}")
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['name'], f"phishing-{self.unique_id}")
     
     def test_bannedword_api(self):
         """Test BannedWord API."""
